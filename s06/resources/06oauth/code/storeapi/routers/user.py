f1e601d0ba6f8fdc177723ccb77617017b8ca226
umer2050 <umb2050@gmail.com>
2025-09-28 05:08:23 +0000

    unzipped 06oauth

---
+++ s06/resources/06oauth/code/storeapi/routers/user.py
@@ -0,0 +1,40 @@
+import logging
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from storeapi.database import database, user_table
+from storeapi.models.user import UserIn
+from storeapi.security import (
+    authenticate_user,
+    create_access_token,
+    get_password_hash,
+    get_user,
+)
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+
+
+@router.post("/register", status_code=201)
+async def register(user: UserIn):
+    if await get_user(user.email):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="A user with that email already exists",
+        )
+    hashed_password = get_password_hash(user.password)
+    query = user_table.insert().values(email=user.email, password=hashed_password)
+
+    logger.debug(query)
+
+    await database.execute(query)
+    return {"detail": "User created."}
+
+
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = await authenticate_user(form_data.username, form_data.password)
+    access_token = create_access_token(user.email)
+    return {"access_token": access_token, "token_type": "bearer"}
